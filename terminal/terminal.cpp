a6d32f3c60baf16b95d03883b5c1fb92bd137d36
Ryan <ryanskelton2000@yahoo.com>
2021-09-22 23:26:59 +0100

    added set cursor function
    declared get_cursor function and not declared
    
    added set function that allows to set ansii without text

--- terminal/terminal.cpp
+++ terminal/terminal.cpp
@@ -23,6 +23,38 @@ std::string terminal::_format(const char* ansii) const {
     return rs;
 }
 
+void terminal::_get_cursor() const {
+}
+
+void terminal::_set_cursor(const uint8_t& row, const uint8_t& col) const {
+    action(control::CURSOR_HOME);
+    action(bind(control::CURSOR_DOWN_N, row));
+    action(bind(control::CURSOR_RIGHT_N, col));
+}
+
+void terminal::_set(const char* ansii, ...) const {
+    va_list lst;
+    std::string rs;
+
+    va_start(lst, ansii);
+
+    while(std::regex_match(ansii, GRAPHIC_REGEX)) {
+        rs += std::string(ansii);
+        ansii = va_arg(lst, const char*);
+
+        if(!ansii)
+            ansii = "!";
+    }
+
+    rs[rs.size()-1] = '\0';
+    rs = _format(rs.c_str());
+    printf("%s", rs.c_str());
+}
+
+/**
+ * @brief used for control ansii escxape characters 
+ * @param ansii code inserted into function to control terminal
+ */
 void terminal::_action(const char* ansii) {
     std::string rs = _format(ansii);
     printf("%s", rs.c_str());
@@ -43,7 +75,8 @@ void terminal::_print(const char* txt, const char* ansii, ...) const {
     va_end(lst);
 
     rs[rs.size()-1] = '\0';
-    
     rs = _format(rs.c_str());
     printf("%s%s", rs.c_str(), txt);
+
+    set(graphics::RESET);
 }
--- terminal/terminal.h
+++ terminal/terminal.h
@@ -8,18 +8,25 @@
 #include <string.h>
 #include <stdarg.h>
 
-#define GRAPHIC_REGEX            std::regex("^[0-9;]+$")
-#define CONTROL_REGEX            std::regex("^[a-zA-Z0-9#?=]+$")
-#define BIND_REGEX               std::regex("#[A-Z]{1}")
+#define BIND_REGEX                                                          std::regex("#[A-Z]{1}")
+#define GRAPHIC_REGEX                                                       std::regex("^[0-9;]+$")
+#define CONTROL_REGEX                                               std::regex("^[a-zA-Z0-9#?=]+$")
 
-#define log                      util::terminal::get_instance()->_print
-#define action(__control__)      util::terminal::get_instance()->_action(__control__)
-#define bind_(__ansii__, __amt__) util::u_bind(__ansii__, __amt__).constrct()
+#define log                                                  util::terminal::get_instance()->_print
+#define set                                                    util::terminal::get_instance()->_set
+#define action(__control__)                    util::terminal::get_instance()->_action(__control__)
+#define bind(__ansii__, __amt__)                        util::u_bind(__ansii__, __amt__).constrct()
+#define set_cursor(__row__, __col__)  util::terminal::get_instance()->_set_cursor(__row__, __col__)
 
 using namespace util;
 using namespace util::graphics;
 
 namespace util {
+
+    struct pos {
+        uint16_t x;
+        uint16_t y;
+    };
 
      struct u_bind {
             size_t      m_amt;
@@ -50,6 +57,9 @@ namespace util {
         std::string _format(const char* ansii) const;
 
     public:
+        void _get_cursor() const;
+        void _set_cursor(const uint8_t& row, const uint8_t& col) const;
+        void _set(const char* ansii, ...) const;
         void _action(const char* ansii);
         void _print(const char* txt, const char* ansii, ...) const;
 
